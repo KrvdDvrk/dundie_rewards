1e720a55c79be88ccdb4a177cd77f0adea471b79
KrvdDvrk <johansson_neves_1995@hotmail.com>
2023-12-08 19:23:15 -0300

    last week update

--- integration/test_load.py
+++ integration/test_load.py
@@ -14,9 +14,6 @@ cmd = CliRunner()
 @pytest.mark.medium
 def test_load_positive_call_load_command():
     """test command load"""
-    out = check_output(["dundie", "load", "tests/assets/people.csv"]).decode("utf-8").split("\n")
-    # breakpoint
-    assert len(out) == 2
     out = cmd.invoke(load, PEOPLE_FILE)
     assert "Dunder Mifflin Associates" in out.output
 
@@ -26,12 +23,6 @@ def test_load_positive_call_load_command():
 @pytest.mark.parametrize("wrong_command", ["loady", "carrega", "start"])
 def test_load_negative_call_load_command_with_wrong_params(wrong_command):
     """test command load"""
-    with pytest.raises(CalledProcessError) as error:
-        check_output(["dundie", "wrong_command", "tests/assets/people.csv"]).decode("utf-8").split(
-            "\n"
-        )
-
-    assert "status 2" in str(error.getrepr())
     out = cmd.invoke(main, wrong_command, PEOPLE_FILE)
     assert out.exit_code != 0
     assert f"No such command '{wrong_command}'." in out.output
